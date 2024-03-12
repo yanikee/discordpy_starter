5e7151d56a260391e2b128f4c782683041758055
やにけ yanike <yanike.82k@gmail.com>
2024-03-12 00:53:16 +0000

    [edit]main.py
    最初にあったら嬉しいものを記述した

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from discord.ext import commands
+
+
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+TOKEN = "ENTER YOUR TOKEN"
+
+
+@bot.event
+async def on_ready():
+  custom_activity = discord.Game(f"ゲームバーのメッセージを記入")
+  await bot.change_presence(status=discord.Status.online,activity=custom_activity)
+
+  # スラッシュコマンドの同期
+  await bot.tree.sync()
+
+  print(f"login as {bot.user.mention}")
+
+
+
+@app_commands.command(name="command_1", description="description")
+async def command_1(self, interaction: discord.Interaction):
+  await interaction.response.send_message("response message", ephemeral=True)
+
+
+# ログいっぱいだとウザいのでWARNING以上にしてます。
+bot.run(TOKEN, log_level = logging.WARNING)
